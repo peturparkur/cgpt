f4469ecdae364b6e2c780c7f90ba452cf26ea9bb
Peter Nagymathe <peter.nagymathe@gmail.com>
2023-03-29 08:25:32 +0100

    split logic into subcommands

---
+++ README.md
@@ -0,0 +1,4 @@
+# How to use
+
+1. Build the project: `cargo build . --release`
+2. set environment variables `CGPT_TOKEN, SAVE_PATH` or save it in `.env` that is in the same folder as the application
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::future::Future;
+use std::{future::Future, path::{Path, PathBuf}, fmt::{Debug, Display}};
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
+use serde::{Serialize, Deserialize};
 use serde_json::json;
 use tokio;
 use types::Message;
@@ -14,49 +15,139 @@ struct Cli {
     chat_id: Option<String>,
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let dote = dotenv::dotenv().ok();
-    let args = Cli::parse();
-    // Only have save_path if we have an id; otherwise it's none (so we can have empty context)
-    let save_path = args
-        .chat_id
-        .as_ref()
-        .map(|id| std::env::var("SAVE_PATH").unwrap() + &id + ".json");
-    let token = std::env::var("CGPT_TOKEN").unwrap();
-    println!("{:?}", &args);
-
-    let msg = types::Message {
-        role: types::Role::User,
-        content: args.message,
-    };
-
-    // Load historic_conversion or set it to empty
-    // empty if no save_path OR no save_file
-    let mut previous_chat = match &save_path {
-        Some(p) => tokio::fs::read(&p).await.map_or(vec![], |y| {
-            serde_json::from_slice::<Vec<Message>>(&y).unwrap()
-        }),
-        _ => vec![],
-    };
-    previous_chat.push(msg);
-
-    let answer = chat_gpt(&previous_chat, &token).await.unwrap();
-    previous_chat.push(TryInto::<Message>::try_into(answer.clone()).unwrap());
-    println!("ChatGPT: \n{}", &previous_chat.last().unwrap().content);
-
-    match &save_path {
-        Some(p) => save_json(&previous_chat, &p).await.unwrap(),
-        _ => (),
-    };
-    return Ok(());
+#[derive(Debug, Parser)]
+#[command(about = "CLI app for easy calling of ChatGPT")]
+struct CliArgs {
+    #[command(subcommand)]
+    command: Commands
 }
 
-async fn save_json<T, P>(content: &T, path: P) -> Result<(), std::io::Error>
+#[derive(Debug, Subcommand)]
+enum Commands {
+    #[command(arg_required_else_help = true, about = "opens a previous conversation")]
+    Checkout {
+        #[arg(short = 'i', long = "id")]
+        id: String,
+        #[arg(short = 'p', long = "prompt")]
+        prompt: Option<String>
+    },
+    #[command(arg_required_else_help = true, about = "continue conversation")]
+    Message {
+        #[arg(short = 'm', long = "message")]
+        message: String, 
+        #[arg(short = 's', long = "nosave")]
+        no_save: Option<bool>
+    },
+    #[command(arg_required_else_help = true, about = "send message without history")]
+    Ask {
+        #[arg(short = 'm', long = "message")]
+        message: String,
+        #[arg(short = 'p', long = "prompt")]
+        prompt: Option<String>
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct Configuration {
+    save_path: PathBuf,
+    current_chat: String,
+}
+impl Default for Configuration {
+    fn default() -> Self {
+        let binding = std::env::var("HOME").unwrap();
+        let p = Path::new(&binding);
+        let fp = p.join(PathBuf::from(".config/cgpt/conversations"));
+        Self {
+            save_path: fp,
+            current_chat: "chat".to_string()
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    // let dote = dotenv::dotenv().ok(); // Only for debugging
+    let binding = std::env::var("HOME").unwrap();
+    let base_path = Path::new(&binding);
+    let config_path = base_path.join(PathBuf::from(".config/cgpt/.cf"));
+    let cfg = match confy::load_path::<Configuration>(&config_path) {
+        Ok(x) => Ok(x),
+        Err(confy::ConfyError::ReadConfigurationFileError(x)) => Ok(Configuration::default()),
+        Err(x) => Err(x)
+    }
+    .unwrap();
+    println!("configuration: {:?}", &cfg);
+    let token = std::env::var("CGPT_TOKEN").unwrap();
+
+
+
+    let clap_args = CliArgs::parse();
+    let y = match clap_args.command {
+        Commands::Checkout { id, prompt } => {
+            confy::store_path(&config_path, Configuration{save_path: cfg.save_path, current_chat: id}).unwrap();
+            ()
+        },
+        Commands::Message { message, no_save } => {
+            let msg = Message{role: types::Role::User, content: message};
+            let context: Vec<Message> = vec![];
+            ()
+        },
+        Commands::Ask { message, prompt } => {
+            let msg = Message{role: types::Role::User, content: message};
+            let background = prompt
+                .map(|x| Message{role: types::Role::System, content: x})
+                .map_or(vec![msg.clone()], |x| vec![x, msg.clone()]);
+            let answer = chat_gpt(&background, &token).await.unwrap();
+            println!("ChatGPT: \n{}", &answer.choices.first().unwrap().message.content);
+            ()
+        }
+    };
+    return Ok(())
+        // let args = Cli::parse();
+
+        // // Only have save_path if we have an id; otherwise it's none (so we can have empty context)
+        // let save_path = args
+        //     .chat_id
+        //     .as_ref()
+        //     .map(|id| Path::new(&cfg.save_path.clone()).join(id.clone() + ".json"));
+
+        // let token = std::env::var("CGPT_TOKEN").unwrap();
+        // println!("{:?}", &args);
+
+        // let msg = types::Message {
+        //     role: types::Role::User,
+        //     content: args.message,
+        // };
+
+        // // Load historic_conversion or set it to empty
+        // // empty if no save_path OR no save_file
+        // let mut previous_chat = match &save_path {
+        //     Some(p) => tokio::fs::read(&p).await.map_or(vec![], |y| {
+        //         serde_json::from_slice::<Vec<Message>>(&y).unwrap()
+        //     }),
+        //     _ => vec![],
+        // };
+        // previous_chat.push(msg);
+
+        // let answer = chat_gpt(&previous_chat, &token).await.unwrap();
+        // previous_chat.push(TryInto::<Message>::try_into(answer.clone()).unwrap());
+        // println!("ChatGPT: \n{}", &previous_chat.last().unwrap().content);
+
+        // match &save_path {
+        //     Some(p) => save_json(&previous_chat, &p).await.unwrap(),
+        //     _ => (),
+        // };
+        // return Ok(());
+}
+
+async fn save_json<T, P>(content: &T, path: &P) -> Result<(), std::io::Error>
 where
     T: serde::Serialize, // Clone
-    P: AsRef<std::path::Path>,
+    P: AsRef<std::path::Path> + Debug,
 {
+    println!("Attempt saving to: {:?}", &path);
+    let _tmp = tokio::fs::create_dir(path).await?;
     return tokio::fs::write(path, serde_json::to_string(&content).unwrap()).await;
 }
 
